53c677fd92e3824488573cde58b30bccdb5d969c
Josiel Queiroz, JR <josielqueirozjr@icloud.com>
2023-09-21 21:45:44 -0300

    update

--- application/main.py
+++ application/main.py
@@ -1,10 +1,10 @@
 from services.database import session
-from models import Pessoa
+from models import *
 from utils.database_utils import create_db
 # python3 -m pip install pymysql sqlalchemy sqlalchemy-utils
 
 if __name__ == "__main__":
-    print("Criando o Banco de Dados!!")
+    print("Criando o Banco de Dados!")
     create_db()
     
     # Mostrar a tabela selecionada, tipo o SELECT no SQL
--- application/services/database.py
+++ application/services/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from urllib.parse import quote
 
-instance = f"mysql+pymysql://root:{quote('2n#*uB?w!r_O')}@localhost:3306/aunimal_hotelPet"
+instance = f"mysql+pymysql://root:{quote('2n#*uB?w!r_O')}@localhost:3306/hotelPet"
 
 if not database_exists(url=instance):
     create_database(url=instance)
