3e18b4be6e4a6ad29d89be66e8f97ade3ad39cd6
Josiel Queiroz, JR <josielqueirozjr@icloud.com>
2023-10-08 00:09:27 -0300

    update cliente.py
    métodos

--- application/models/cliente.py
+++ application/models/cliente.py
@@ -1,6 +1,6 @@
 from models import Base, Pessoa 
 from sqlalchemy import ForeignKey, DATETIME
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.dialects.mysql import INTEGER
 from datetime import datetime
 
@@ -8,13 +8,12 @@ from datetime import datetime
 class Cliente(Base):
     __tablename__ = "cliente"
     
-    id_cliente: Mapped[int] = mapped_column("id_cliente", INTEGER, ForeignKey(Pessoa.id_pessoa), nullable=False, autoincrement=True, primary_key=True)
+    id_cliente: Mapped[int] = mapped_column("id_cliente", INTEGER, nullable=False, autoincrement=True, primary_key=True)
     id_pessoa = Mapped[int] = mapped_column("id_pessoa", INTEGER, ForeignKey(Pessoa.id_pessoa), nullable=False)
     data_criacao: Mapped[datetime] = mapped_column(DATETIME, nullable=False, default=datetime.now())
-    id_pessoa = Column(Integer, ForeignKey('associado.id_pessoa'), nullable=False)
     
-    # Adicione um relacionamento para acessar os dados do associado
-    pessoa = relationship('pessoa', backref='cliente')
+    # Relacionamento para acessar os dados de pessoa
+    pessoa = relationship('Pessoa', backref='cliente')
 
     def __init__(self, id_pessoa):
         self.id_pessoa = id_pessoa
