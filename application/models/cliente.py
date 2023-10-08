256df511d37643b45671070c00f9145c99160117
Josiel Queiroz, JR <josielqueirozjr@icloud.com>
2023-10-07 23:57:50 -0300

    update

--- application/models/cliente.py
+++ application/models/cliente.py
@@ -9,5 +9,13 @@ class Cliente(Base):
     __tablename__ = "cliente"
     
     id_cliente: Mapped[int] = mapped_column("id_cliente", INTEGER, ForeignKey(Pessoa.id_pessoa), nullable=False, autoincrement=True, primary_key=True)
+    id_pessoa = Mapped[int] = mapped_column("id_pessoa", INTEGER, ForeignKey(Pessoa.id_pessoa), nullable=False)
     data_criacao: Mapped[datetime] = mapped_column(DATETIME, nullable=False, default=datetime.now())
-   
+    id_pessoa = Column(Integer, ForeignKey('associado.id_pessoa'), nullable=False)
+    
+    # Adicione um relacionamento para acessar os dados do associado
+    pessoa = relationship('pessoa', backref='cliente')
+
+    def __init__(self, id_pessoa):
+        self.id_pessoa = id_pessoa
+  
